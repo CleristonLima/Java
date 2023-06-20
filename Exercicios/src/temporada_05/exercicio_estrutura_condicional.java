fc459d6f43a10ad8fa9c9bd2aa036f4d648ffd99
cleri <cleri@192.168.1.8>
2023-06-19 22:36:02 -0300

    Meus_Exercicios

--- Exercicios/src/temporada_05/exercicio_estrutura_condicional.java
+++ Exercicios/src/temporada_05/exercicio_estrutura_condicional.java
@@ -9,7 +9,7 @@ public class exercicio_estrutura_condicional {
 		// 1 -> Fazer um programa para ler um numero inteiro, e depois dizer se este numero é negativo ou não
 		Scanner sc = new Scanner(System.in);
 		
-		/*int numero;
+		int numero;
 		
 		System.out.println("Por favor digite um numero: ");
 		numero = sc.nextInt();
@@ -21,11 +21,11 @@ public class exercicio_estrutura_condicional {
 		} else {
 			
 			System.out.println("NAO NEGATIVO");
-		}*/
+		}
 		
 		// 2 - Fazer um programa para ler um numero inteiro e dizer se este numero é PAR ou IMPAR
 		
-		/*int numero;
+		int numero;
 		
 		System.out.println("Por favor digite um numero: ");
 		numero = sc.nextInt();
@@ -37,12 +37,12 @@ public class exercicio_estrutura_condicional {
 		} else {
 			
 			System.out.println("IMPAR");
-		}*/
+		}
 		
 		//  3 - Leia 2 valores inteiro (A e B). Apos, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Não sao Multiplos", indicando se os valoes lidos são multiplos entre si.
 		   //  Atenção: os numero devem podem ser digitados em ordem crescente ou decres
 		
-		/*int A, B;
+		int A, B;
 		
 		System.out.println("Digite um valor de A: ");
 		A = sc.nextInt();
@@ -57,7 +57,7 @@ public class exercicio_estrutura_condicional {
 		} else {
 			
 			System.out.println("Nao Sao Multiplos");
-		} */
+		} 
 		
 		// 4 -> Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia
 		//      e terminar em outro, tendo uma duração minima de 1 hora e maxima de 24 horas.
