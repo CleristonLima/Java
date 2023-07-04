aad868bbd80e6944d7b9db14bb80d8fbd77325e5
Cleriston Lima <87100328+CleristonLima@users.noreply.github.com>
2023-07-04 19:11:48 -0300

    Add files via upload

---
+++ Operadores_Bitwise/src/operadorBit_A_Bit/operadorBitaBit.java
@@ -0,0 +1,15 @@
+package operadorBit_A_Bit;
+
+public class operadorBitaBit {
+
+	public static void main(String[] args) {
+		
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+
+	}
+
+}
