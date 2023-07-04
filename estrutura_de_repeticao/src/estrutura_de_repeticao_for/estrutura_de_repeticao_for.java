d2b28dc69ef6ea1d38168f9e84544e2d747da47a
Cleriston Lima <87100328+CleristonLima@users.noreply.github.com>
2023-07-04 19:10:10 -0300

    Add files via upload

---
+++ estrutura_de_repeticao/src/estrutura_de_repeticao_for/estrutura_de_repeticao_for.java
@@ -0,0 +1,28 @@
+package estrutura_de_repeticao_for;
+
+import java.util.Scanner;
+
+public class estrutura_de_repeticao_for {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int soma = 0;
+		
+		for (int i=0; i<N; i++) {
+			
+			int x = sc.nextInt();
+			soma = soma + x;
+			
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
+
+	}
+
+}
